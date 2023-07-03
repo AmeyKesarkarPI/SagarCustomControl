e56cb9a02940b898b68ba4521f942649e24236c3
ameykesarkar <amey.kesarkar@pitechniques.com>
2023-07-03 11:26:22 +0530

    Add project files.

---
+++ SagarCustomControl/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SagarCustomControl
+{
+    public class Class1
+    {
+
+
+        public static int GetMyProperty(DependencyObject obj)
+        {
+            return (int)obj.GetValue(MyPropertyProperty);
+        }
+
+        public static void SetMyProperty(DependencyObject obj, int value)
+        {
+            obj.SetValue(MyPropertyProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MyPropertyProperty =
+            DependencyProperty.RegisterAttached("MyProperty", typeof(int), typeof(Class1), new PropertyMetadata(0));
+
+
+    }
+}
---
+++ SagarCustomControl/OnlyABCD.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SagarCustomControl
+{
+    /// <summary>
+    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+    ///
+    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:SagarCustomControl"
+    ///
+    ///
+    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:SagarCustomControl;assembly=SagarCustomControl"
+    ///
+    /// You will also need to add a project reference from the project where the XAML file lives
+    /// to this project and Rebuild to avoid compilation errors:
+    ///
+    ///     Right click on the target project in the Solution Explorer and
+    ///     "Add Reference"->"Projects"->[Browse to and select this project]
+    ///
+    ///
+    /// Step 2)
+    /// Go ahead and use your control in the XAML file.
+    ///
+    ///     <MyNamespace:OnlyABCD/>
+    ///
+    /// </summary>
+    public class OnlyABCD : TextBox
+    {
+
+
+        public Action<string> OnEnterPress
+        {
+            get { return (Action<string>)GetValue(OnEnterPressProperty); }
+            set { SetValue(OnEnterPressProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for OnEnterPress.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty OnEnterPressProperty =
+            DependencyProperty.Register("OnEnterPress", typeof(Action<string>), typeof(OnlyABCD));
+
+
+        protected override void OnKeyUp(KeyEventArgs e)
+        {
+            base.OnKeyUp(e);
+            if (e.Key == Key.Enter)
+            {
+                OnEnterPress?.Invoke(this.Text);
+            }
+        }
+
+
+    }
+}
